3bbd30eafae0fe5c1fee96281fd0935d81eb1903
Ishita Mediratta <ishitamediratta19@gmail.com>
2020-11-10 02:46:51 +0530

    Update arelu.py

--- arelu.py
+++ arelu.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 class ApproxRelu(Layer):
-
+    
     def __init__(self, k=0.5, n=1.1, **kwargs):
         super(ApproxRelu, self).__init__(**kwargs)
         self.supports_masking = True
@@ -9,12 +9,10 @@ class ApproxRelu(Layer):
         self.n = K.cast_to_floatx(n)
 
     def call(self, inputs):
-
         orig = inputs
         inputs = tf.where(orig <= 0.0, tf.zeros_like(inputs), inputs)
         inputs = tf.where(tf.greater(orig, 0.0), self.k*tf.pow(inputs, self.n), inputs)
         return  inputs
-
 
     def get_config(self):
         config = {'k': float(self.k), 'n': float(self.n)}
